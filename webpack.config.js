03612261726395cc40403d5ed696d000c0084165
Maxar96 <61992296+Maxar96@users.noreply.github.com>
2020-04-28 12:59:10 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+
+module.exports = [{
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+},
+{
+  entry: './style.scss',
+  output: {
+    // This is necessary for webpack to compile
+    // But we never use style-bundle.js
+    filename: 'style-bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
+          },
+          { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer()]
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              // Prefer Dart Sass
+              implementation: require('sass'),
+              sassOptions: {
+                includePaths: ['./node_modules']
+              },
+            }
+          },
+        ]
+      }
+    ]
+  },
+}];
